837ceed8a2b92cbd691337b34c80f01125f79f04
Ron Ganguly <c1ph3r9@gmail.com>
2019-02-18 07:24:36 -0600

    add image upload header and router config v-1

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+
+<template>
+<div class="ui secondary pointing menu">
+  <router-link to="/" class="active item">Image Storage</router-link>
+ 
+  <div class="right menu">
+    <div class="horizontal" v-if="isLoggedIn">
+      <router-link to="/" href="#" class="item">Galleries</router-link>
+      <router-link to='/upload' href="#" class="item">Upload</router-link>
+      <a href="#" class="item" @click="logout">Logout</a>
+    </div>
+    <a v-else href="#" class="ui item" @click="login"> Login</a>
+
+
+  </div>
+
+</div>
+</template>
+
+
+
+<script>
+import {mapActions, mapGetters} from 'vuex'
+export default {
+  name:"AppHeader",
+  methods:{...mapActions(['login','logout'])
+  
+  },
+  computed:mapGetters(['isLoggedIn'])
+}
+</script>
+
+<style scoped>
+  .horizontal{
+    display: flex;
+    flex-direction: row;
+  }
+
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue  from "vue";
+import VueRouter from 'vue-router'
+import App from './App';
+import store from './store'
+import AuthHandler from './components/AuthHandler.vue'
+import ImageList from './components/ImageList.vue'
+import UploadForm from './components/UploadForm.vue'
+Vue.use(VueRouter);
 
-Vue.config.productionTip = false
+//configuration routes
+export const router = new VueRouter({
+  mode:'history',
+  routes:[
+    { path: '/oauth2/callback', component:AuthHandler },
+    {path:'/', component: ImageList},
+    {path:"/upload", component:UploadForm}
+
+  ]
+})
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store:store,
+  render:(h)=>h(App)
+}).$mount("#app")
